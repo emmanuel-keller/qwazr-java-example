307fa6a43bc00fa6c00e2b1748eaaefb46a8f30d
Emmanuel Keller <ekeller@open-search-server.com>
2016-07-28 12:07:04 +0200

    The project works with QWAZR 1.0.1

--- src/main/java/com/qwazr/example/HomeServlet.java
+++ src/main/java/com/qwazr/example/HomeServlet.java
@@ -1,7 +1,7 @@
 package com.qwazr.example;
 
+import com.qwazr.library.annotations.Library;
 import com.qwazr.tools.FreeMarkerTool;
-import com.qwazr.tools.ToolsManager;
 import freemarker.template.TemplateException;
 
 import javax.servlet.ServletException;
@@ -10,12 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class RootServlet extends HttpServlet {
+public class HomeServlet extends HttpServlet {
 
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-					throws ServletException, IOException {
-		ToolsManager tools = (ToolsManager) request.getAttribute("tools");
-		FreeMarkerTool freemarker = tools.get("freemarker");
+	@Library("freemarker")
+	protected FreeMarkerTool freemarker;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		try {
 			request.setAttribute("my_var", "Hello World! Java Servlet example.");
 			freemarker.template("src/views/index.ftl", request, response);
--- src/views/index.ftl
+++ src/views/index.ftl
@@ -3,6 +3,6 @@
     <link href="${request.contextPath}/css/example.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-<h1>${request.attributes.my_var}</h1>
+<h1>${request.getAttribute('my_var')}</h1>
 </body>
 </html>
